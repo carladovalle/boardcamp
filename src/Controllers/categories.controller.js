b335ac1ec0cf947db32ff2b311610edec41e61b8
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-10-01 17:54:31 -0300

    feat: does not allow the creation of categories with repeated names

--- src/Controllers/categories.controller.js
+++ src/Controllers/categories.controller.js
@@ -22,6 +22,10 @@ async function createCategories (req, res) {
     const { name } = req.body;
 
     try {
+        const hasCategory = await connection.query('SELECT * FROM categories WHERE name = $1;', [name]);
+        if (hasCategory) {
+            return res.status(409).send("A categoria já existe.")
+        }
         await connection.query(
             'INSERT INTO categories (name) VALUES ($1);', [name]
         );
