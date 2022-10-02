508e91009af6e2a5c8aaa7c6dd375e12a6ab0f6e
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-10-01 21:22:25 -0300

    feat: insert validations with joi in the creation of customer data

--- src/Controllers/customers.controller.js
+++ src/Controllers/customers.controller.js
@@ -1,4 +1,5 @@
 import connection from "../Database/database.js";
+import { customersSchema } from "../Schemas/customersValidation.js";
 
 async function getCustomers (req, res) {
     const customers = await connection.query('SELECT * FROM customers;');
@@ -13,25 +14,64 @@ async function viewCustomers (req, res) {
 }; 
 
 async function createCustomers (req, res) {
+
+    const validation = customersSchema.validate(req.body, {
+        abortEarly: false,
+    });
+
+    if (validation.error) {
+        const errorList = validation.error.details
+          .map((err) => err.message)
+          .join('\n');
+        return res.status(400).send(errorList);
+    }
+
     const { name, phone, cpf, birthday } = req.body;
 
-    await connection.query(
-        'INSERT INTO customers (name, "phone", "cpf", "birthday") VALUES ($1, $2, $3, $4);', 
-            [name, phone, cpf, birthday]
-    );
+    try {
 
-    res.sendStatus(201);
+        await connection.query(
+            'INSERT INTO customers (name, "phone", "cpf", "birthday") VALUES ($1, $2, $3, $4);', 
+                [name, phone, cpf, birthday]
+        );
+    
+        res.sendStatus(201);
+
+    } catch (error) {
+        return res.send(error.message);
+    }
+
 };
 
 async function updateCustomers (req, res) {
+
+    const validation = customersSchema.validate(req.body, {
+        abortEarly: false,
+    });
+
+    if (validation.error) {
+        const errorList = validation.error.details
+          .map((err) => err.message)
+          .join('\n');
+        return res.status(400).send(errorList);
+    }
+
     const { name, phone, cpf, birthday } = req.body;
+
     const { id } = req.params;
 
-    await connection.query(
-        `UPDATE customers SET ("name", "phone", "cpf", "birthday") = ('${name}', '${phone}', '${cpf}', '${birthday}') WHERE id = $1;`, [id]
-    );
+    try {
 
-    res.sendStatus(200);
+        await connection.query(
+            `UPDATE customers SET ("name", "phone", "cpf", "birthday") = ('${name}', '${phone}', '${cpf}', '${birthday}') WHERE id = $1;`, [id]
+        );
+    
+        res.sendStatus(200);
+
+    } catch (error) {
+        return res.send(error.message);
+    }
+
 };
 
 export { getCustomers, viewCustomers, createCustomers, updateCustomers };
