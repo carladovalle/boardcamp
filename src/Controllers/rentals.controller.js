e682262e0d82aeb4207b150c9bb88c20b89b1222
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-10-03 16:38:29 -0300

    feat: add some validations on rentals

--- src/Controllers/rentals.controller.js
+++ src/Controllers/rentals.controller.js
@@ -52,12 +52,32 @@ async function createRentals (req, res) {
 
     const today = dayjs().format('YYYY-MM-DD');
 
-    const rental = req.body;
+    const { customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee } = req.body;
 
     try {
 
+        const game = await db.query(`
+        SELECT * FROM games
+        WHERE id = $1;
+        `,[gameId]);
+
+        const customers = await db.query(`
+        SELECT * FROM customers
+        WHERE id = $1;
+        `,[customerId]);
+
+        if (game.rows.length === 0 || customers.rows.length === 0){
+            return res.sendStatus(400);
+        }
+
+        const price = game.rows[0].pricePerDay;
+
+        if (daysRented < 0) {
+            return res.sendStatus(400);
+        }
+
         await connection.query(
-            `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, null, $5, null);`, [rental.customerId, rental.gameId, today, rental.daysRented, rental.originalPrice]
+            `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, null, $5, null);`, [customerId, gameId, today, daysRented, (parseInt(price) * parseInt(rental.daysRented))]
         );
 
         res.sendStatus(201);
